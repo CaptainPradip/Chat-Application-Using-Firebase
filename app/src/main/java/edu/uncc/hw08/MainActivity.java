948f6b383b7613d1229785d83a3caa966f206af0
CaptainPradip <pradipnemane@gmail.com>
2022-11-13 10:43:45 -0500

    removed intent

--- app/src/main/java/edu/uncc/hw08/MainActivity.java
+++ app/src/main/java/edu/uncc/hw08/MainActivity.java
@@ -2,28 +2,20 @@ package edu.uncc.hw08;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import edu.uncc.hw08.models.User;
-
 public class MainActivity extends AppCompatActivity implements MyChatsFragment.MyChatsListener,
         CreateChatFragment.CreateChatListener {
-
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (getIntent().hasExtra("user") && getIntent().getSerializableExtra("user") != null) {
-            User user = (User) getIntent().getSerializableExtra("user");
-            Log.d(TAG, "onCreate: " + user);
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.rootView, MyChatsFragment.newInstance(user))
-                    .commit();
-        }
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.rootView, new MyChatsFragment())
+                .commit();
     }
 
     @Override
